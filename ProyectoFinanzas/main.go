365bd651129cd69f465a69c52105fa706841c286
NachoAsti24 <iastigueta@alumnos.exa.unicen.edu.ar>
2025-10-27 16:19:05 -0300

    Act handler /Movimientos/

--- ProyectoFinanzas/main.go
+++ ProyectoFinanzas/main.go
@@ -2,13 +2,18 @@ package main
 
 import (
 	//"context"
+
 	"database/sql"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
+	"strings"
+
+	sqlc "ProyectoFinanzas/db/sqlc"
 
 	_ "github.com/lib/pq"
-	//sqlc "ProyectoFinanzas/db/sqlc"
 )
 
 func main() {
@@ -20,9 +25,12 @@ func main() {
 	}
 	defer db.Close() //Cerrar conexion
 
-	//queries := sqlc.New(db)
+	queries := sqlc.New(db)
 	//ctx := context.Background()
 
+	http.HandleFunc("/movimientos/", func(w http.ResponseWriter, r *http.Request) {
+		movimientosHandler(w, r, queries)
+	})
 	//Abrir el servidor
 	staticDir := "./static"
 
@@ -38,6 +46,67 @@ func main() {
 
 	if err != nil {
 		fmt.Printf("Error: %s\n", err)
+	}
+}
+
+func movimientosHandler(w http.ResponseWriter, r *http.Request, queries *sqlc.Queries) {
+	parts := strings.Split(r.URL.Path, "/")
+	if len(parts) != 3 {
+		http.Error(w, "Invalid URL", http.StatusBadRequest)
+		return
+	}
+	id, err := strconv.Atoi(parts[2])
+	if err != nil {
+		http.Error(w, "Id de movimiento invalido", http.StatusBadRequest)
+		return
+	}
+	switch r.Method {
+	case http.MethodGet:
+		getMov(w, r, id, queries)
+	case http.MethodPut:
+		updateMov(w, r, id, queries)
+	case http.MethodDelete:
+		deleteMov(w, r, id, queries)
+	default:
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+	}
+}
+
+func getMov(w http.ResponseWriter, r *http.Request, id int, queries *sqlc.Queries) {
+	movimiento, err := queries.GetMovimiento(r.Context(), int32(id))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusNotFound)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(movimiento)
+}
+
+func updateMov(w http.ResponseWriter, r *http.Request, id int, queries *sqlc.Queries) {
+	var aux sqlc.UpdateMovimientoParams
+	var mov sqlc.Movimiento
+	err := json.NewDecoder(r.Body).Decode(&aux)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	mov, err = queries.UpdateMovimiento(r.Context(), aux)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusNotFound)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(mov)
+}
+
+func deleteMov(w http.ResponseWriter, r *http.Request, id int, queries *sqlc.Queries) {
+	err := queries.DeleteMovimiento(r.Context(), int32(id))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusNotFound)
+		return
 	}
 }
 
